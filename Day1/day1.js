94b6a52979eee8d189feca17e032f98fa1441674
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-01 19:23:20 +0530

    Day1 Lvl 1

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,1 +1,27 @@
-console.log('hello AOC 2023');
+function numberExtractor(str){
+  let firstNumber, lastNumber;
+  for(let i=0; i<str.length; i++){
+    if(Number.isInteger(+str[i])){
+      firstNumber = Number.parseInt(str[i]);
+      break;
+    }
+  }
+  for(let j=str.length; j>=0; j--){
+    if(Number.isInteger(+str[j])) {
+      lastNumber = Number.parseInt(str[j]);
+      break;
+    }
+  }
+  return (firstNumber*10)+lastNumber;
+}
+
+const fs = require('fs');
+fs.readFile('text', 'utf8', (err, data)=>{
+  if(err) throw err;
+  const lines = data.toString().split('\n');
+  let sum = lines.reduce((total, curr) =>{
+    if(curr === '')return total;
+    return total+numberExtractor(curr);
+  },0);
+  console.log('sum: ', sum);
+});
