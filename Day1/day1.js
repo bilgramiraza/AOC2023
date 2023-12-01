d3563a0e4e4a76ca382b55f9f2251bc428d0dbb6
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-01 22:01:49 +0530

    Day1 Lvl 2

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,17 +1,77 @@
+let numbersHashmap = {
+  'one':1,
+  'two':2,
+  'three':3,
+  'four':4,
+  'five':5,
+  'six':6,
+  'seven':7,
+  'eight':8,
+  'nine':9,
+};
+
+function getFirstWordInteger(str){
+  let earlistInteger = {
+    number:0,
+    index:str.length,
+  };
+
+  Object.keys(numbersHashmap).forEach(num =>{
+    if(str.includes(num)){
+      const index = str.indexOf(num)
+      if(index<earlistInteger.index){
+        earlistInteger.number = numbersHashmap[num];
+        earlistInteger.index = index;
+      }
+    }
+  });
+  return earlistInteger;
+}
+
+function getLastWordInteger(str){
+  let latestInteger = {
+    number:0,
+    index:-1,
+  };
+
+  Object.keys(numbersHashmap).forEach(num =>{
+    if(str.includes(num)){
+      const index = str.lastIndexOf(num)
+      if(index>latestInteger.index){
+        latestInteger.number = numbersHashmap[num];
+        latestInteger.index = index;
+      }
+    }
+  });
+  return latestInteger;
+}
+
+function getFirstNumber(str){
+  const firstInteger = +str.match(/\d/);
+  let integerIndex = str.indexOf(firstInteger);
+  if(integerIndex === -1) integerIndex = str.length;
+  const { index:wordIndex, number:wordNumber } = getFirstWordInteger(str);
+  return integerIndex<wordIndex ? firstInteger : wordNumber;
+}
+
+function getLastInterger(str){
+  for(let i=str.length-1; i>=0;i--){
+    if(!isNaN(str[i]))
+      return +str[i];
+  }
+  return 0;
+}
+
+function getLastNumber(str){
+  let lastInteger = getLastInterger(str);
+  let integerIndex = str.lastIndexOf(lastInteger);
+  const { index:wordIndex, number:wordNumber } = getLastWordInteger(str);
+  return integerIndex>wordIndex ? lastInteger : wordNumber;
+}
+
 function numberExtractor(str){
-  let firstNumber, lastNumber;
-  for(let i=0; i<str.length; i++){
-    if(Number.isInteger(+str[i])){
-      firstNumber = Number.parseInt(str[i]);
-      break;
-    }
-  }
-  for(let j=str.length; j>=0; j--){
-    if(Number.isInteger(+str[j])) {
-      lastNumber = Number.parseInt(str[j]);
-      break;
-    }
-  }
+  const firstNumber = getFirstNumber(str);
+  const lastNumber = getLastNumber(str);
   return (firstNumber*10)+lastNumber;
 }
 
