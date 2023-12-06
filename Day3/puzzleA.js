6c0d85b80af9f2a2262aef58b0029e72a33f5d70
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-07 04:09:21 +0530

    Day3 lvl 1

--- Day3/puzzleA.js
+++ Day3/puzzleA.js
@@ -1,47 +1,43 @@
 const fs = require('fs');
 const { performance } = require('perf_hooks');
 
+function getSumOfParts(grid){
+  let sum = 0;  
+  for(let i =0; i<grid.length; i++){
+    let number = '', nearSymbol = false, checkNumber=false;
+    for(let j=0;j<grid[i].length;j++){
+      if(!checkNumber && !isNaN(grid[i][j])){
+        checkNumber = true;
+        number='';
+        nearSymbol = false;
+      }
+      if((j === grid[i].length-1 || isNaN(grid[i][j])) && checkNumber){
+        if(nearSymbol)
+          sum += parseInt(number.concat(!isNaN(grid[i][j]) ? grid[i][j] : ''));
+        checkNumber = false;
+      }
+      if(checkNumber){
+        number = number.concat(grid[i][j]);
+        for(let x=-1; x<2; x++)
+          for(let y=-1; y<2; y++){
+            if(!x && !y) continue;
+            if(i+x<0 || j+y <0 || i+x>=grid.length || j+y>=grid[i].length) continue;
+            if(isNaN(grid[i+x][j+y]) && grid[i+x][y+j] !== '.')
+              nearSymbol = true;
+          }
+      }
+    }
+  }
+  return sum;
+}
+
 fs.readFile('text', 'utf8', (err, data)=>{
   if(err) throw err;
   let startTime = performance.now();
   const lines = data.split('\n').slice(0,-1);
-  let numbers = [];
-  for(let i=0;i<lines.length;i++){
-    let number = '';
-    for(let j=0;j<lines[i].length+1;j++){
-      if(!isNaN(+lines[i][j])){
-        number =number+lines[i][j];
-        continue;
-      }else if(number){
-        const leftLimit = j-number.length -1 <0? 0: j-number.length -1;
-        let symbol = false;
-        if(i>0){
-          symbol = !lines[i-1]
-            .slice(leftLimit, j+1)
-            .split('')
-            .every(char=>char==='.'||!isNaN(+char));
-        }
-        if(!symbol && i<lines.length-1) {
-          symbol = !lines[i+1]
-            .slice( leftLimit, j+1)
-            .split('')
-            .every(char=>char==='.'||!isNaN(+char));
-        }
-        if(!symbol){
-        symbol = !lines[i]
-          .slice( leftLimit, j+1)
-          .split('')
-          .every(char=>char==='.'||!isNaN(+char));
-        }
-        if(symbol){
-          numbers.push(+number);
-        }
-        number = '';
-      }
-    }
-  }
-  const sum = numbers.reduce((sum, curr)=>sum+curr,0);
-  console.log(sum);
+  const sumOfParts = getSumOfParts(lines);
+  console.log(sumOfParts);
   let endTime = performance.now();
   console.log('runtime: ',endTime-startTime, 'ms');
 });
+
