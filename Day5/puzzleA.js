309a25ac2c6acae5f017b2a31df521b7c9771c8e
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-08 11:49:13 +0530

    Day5 lvl 2

--- Day5/puzzleA.js
+++ Day5/puzzleA.js
@@ -25,32 +25,35 @@ function parseData(data){
 }
 
 function mapConverter(input, map){
-  if(input < map[0][0] || input > (map[map.length-1][0]+map[map.length-1][2])){
-    return input;
-  }
+  if(input < map[0][0] || input > (map[map.length-1][0]+map[map.length-1][2]))  return input;
+  
   for(let i=0; i<map.length;i++)
-    if(input>map[i][0] && input <= (map[i][0]+map[i][2]))
+    if(input>=map[i][0] && input <(map[i][0]+map[i][2]))
       return (input-map[i][0])+map[i][1];
-    
 
   return input;
 }
 
-fs.readFile('./text', 'utf8', (err, data)=>{
+fs.readFile('./test', 'utf8', (err, data)=>{
   if(err) throw err;
   let startTime = performance.now();
   const {seeds, maps} = parseData(data);
-  const mappedSeeds = seeds.map(seed=>{
-    const sts = mapConverter(seed, maps[0][1]);
-    const stf = mapConverter(sts, maps[1][1]);
-    const ftw = mapConverter(stf, maps[2][1]);
-    const wtl = mapConverter(ftw, maps[3][1]);
-    const ltt = mapConverter(wtl, maps[4][1]);
-    const tth = mapConverter(ltt, maps[5][1]);
-    const htl = mapConverter(tth, maps[6][1]);
-    return htl;
-  });
-  console.log(Math.min(...mappedSeeds));
+  let lowestLocation = Number.POSITIVE_INFINITY;
+  for(let i=0; i<seeds.length; i+=2){
+    const start = +seeds[i];
+    const range = +seeds[i+1];
+    for(let j=start;j<start+range;j++){
+      const sts = mapConverter(j, maps[0][1]);
+      const stf = mapConverter(sts, maps[1][1]);
+      const ftw = mapConverter(stf, maps[2][1]);
+      const wtl = mapConverter(ftw, maps[3][1]);
+      const ltt = mapConverter(wtl, maps[4][1]);
+      const tth = mapConverter(ltt, maps[5][1]);
+      const htl = mapConverter(tth, maps[6][1]);
+      if(htl<lowestLocation)  lowestLocation = htl;
+    }
+  }
+  console.log(lowestLocation);
   let endTime = performance.now();
   console.log('runtime: ',endTime-startTime, 'ms');
 });
