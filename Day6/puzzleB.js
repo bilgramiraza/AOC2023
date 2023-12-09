c7d0ab83afd39ae873f7ba8fc3722d7824706866
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-09 13:53:39 +0530

    Day6 lvl2

--- Day6/puzzleB.js
+++ Day6/puzzleB.js
@@ -2,15 +2,32 @@ const fs = require('fs');
 const { performance } = require('perf_hooks');
 
 function parseData(data){
-  const lines = data.split('\n').slice(0,-1);
-  return lines;
+  const [time, distance] = data
+    .split('\n')
+    .slice(0,-1)
+    .map(line=>line
+      .split(':'))
+    .map(item=>item[1]
+      .trim()
+      .split(' ')
+      .join(''));
+  return [+time,+distance];
+}
+
+function getRaceResults(time, distance){
+  let winningCount = 0;
+  for(let i=0;i<=time;i++)
+    if(i*(time-i)>distance)
+      winningCount++;
+  return winningCount;
 }
 
 fs.readFile('./text', 'utf8', (err, data)=>{
   if(err) throw err;
   let startTime = performance.now();
-  const lines = parseData(data);
-  console.log(lines);
+  const mergedData = parseData(data);
+  const raceResults = getRaceResults(mergedData[0], mergedData[1]);
+  console.log(raceResults);
   let endTime = performance.now();
   console.log('runtime: ',endTime-startTime, 'ms');
 });
