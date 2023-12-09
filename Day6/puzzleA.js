ca0c013b8d547fab1e95a7444168726aa56dcc2e
bilgramiraza <bilgramiraza@yahoo.com>
2023-12-09 13:48:31 +0530

    Day6 lvl 1

--- Day6/puzzleA.js
+++ Day6/puzzleA.js
@@ -2,15 +2,34 @@ const fs = require('fs');
 const { performance } = require('perf_hooks');
 
 function parseData(data){
-  const lines = data.split('\n').slice(0,-1);
-  return lines;
+  const [time, distance] = data
+    .split('\n')
+    .slice(0,-1)
+    .map(line=>line
+      .split(':'))
+    .map(item=>item[1]
+      .trim()
+      .split(' ')
+      .filter(item=>item));
+  const mergedData = time.map((item, i)=>[item, distance[i]]);
+  return mergedData;
 }
 
-fs.readFile('./text', 'utf8', (err, data)=>{
+function getRaceResults(time, distance){
+  let winningCount = 0;
+  for(let i=0;i<=time;i++)
+    if(i*(time-i)>distance)
+      winningCount++;
+  return winningCount;
+}
+
+fs.readFile('./test', 'utf8', (err, data)=>{
   if(err) throw err;
   let startTime = performance.now();
-  const lines = parseData(data);
-  console.log(lines);
+  const mergedData = parseData(data);
+  const raceResults = mergedData.map(data=>getRaceResults(data[0], data[1]));
+  const totalWinningChances = raceResults.reduce((total, curr)=>total*curr,1);
+  console.log(totalWinningChances);
   let endTime = performance.now();
   console.log('runtime: ',endTime-startTime, 'ms');
 });
